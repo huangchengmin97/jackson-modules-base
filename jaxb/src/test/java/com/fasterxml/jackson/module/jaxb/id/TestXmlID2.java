53b125a4c8e1b600edfe4c3427e08ec48265878b
huangchengmin97 <huangchengmin97@gmail.com>
2021-08-29 18:58:38 -0500

    10

--- jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
+++ jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
@@ -140,8 +140,9 @@ public class TestXmlID2 extends BaseJaxbTest
                 .annotationIntrospector(new JaxbAnnotationIntrospector())
                 .build();
         List<User> users = getUserList();
-        // final String json = mapper.writeValueAsString(users);
-        final String json = mapper.writeValue(users);
+        System.out.print("GfG1");
+        System.out.print(users);
+        final String json = mapper.writeValueAsString(users);
         String expected = "[{\"id\":11,\"username\":\"11\",\"email\":\"11@test.com\",\"department\":9}"
                 +",{\"id\":22,\"username\":\"22\",\"email\":\"22@test.com\",\"department\":9}"
                 +",{\"id\":33,\"username\":\"33\",\"email\":\"33@test.com\",\"department\":null}]";
