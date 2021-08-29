e66037dfb5665da3d9b858ed48a393a64a042fac
huangchengmin97 <huangchengmin97@gmail.com>
2021-08-29 18:41:16 -0500

    6

--- jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
+++ jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
@@ -141,13 +141,10 @@ public class TestXmlID2 extends BaseJaxbTest
                 .build();
         List<User> users = getUserList();
         final String json = mapper.writeValueAsString(users);
-        // String expected = "[{\"id\":11,\"username\":\"11\",\"email\":\"11@test.com\",\"department\":9}"
-        //         +",{\"id\":22,\"username\":\"22\",\"email\":\"22@test.com\",\"department\":9}"
-        //         +",{\"id\":33,\"username\":\"33\",\"email\":\"33@test.com\",\"department\":null}]";
-        String expected = "[{\"id\":11,\"username\":\"11\",\"email\":\"11@test.com\","
-                +"\"department\":{\"id\":9,\"name\":\"department9\",\"employees\":["
-                +"11,{\"id\":22,\"username\":\"22\",\"email\":\"22@test.com\","
-                +"\"department\":9}]}},22,{\"id\":33,\"username\":\"33\",\"email\":\"33@test.com\",\"department\":null}]";
+        String expected = "[{\"id\":11,\"username\":\"11\",\"email\":\"11@test.com\",\"department\":9}"
+                +",{\"id\":22,\"username\":\"22\",\"email\":\"22@test.com\",\"department\":9}"
+                +",{\"id\":33,\"username\":\"33\",\"email\":\"33@test.com\",\"department\":null}]";
+
 
         assertEquals(expected, json);
 
