5a4333dc5d1283be2f65545bcc3a919b7e00ad42
huangchengmin97 <huangchengmin97@gmail.com>
2021-08-29 20:05:41 -0500

    25

--- jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
+++ jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
@@ -143,8 +143,8 @@ public class TestXmlID2 extends BaseJaxbTest
                 .build();
         List<User> users = getUserList();
         System.out.println("!!!!!!!");
-        System.out.println(users);
         final String json = mapper.writeValueAsString(users);
+        System.out.println(json);
         String expected = "[{\"id\":11,\"username\":\"11\",\"email\":\"11@test.com\",\"department\":9}"
                 +",{\"id\":22,\"username\":\"22\",\"email\":\"22@test.com\",\"department\":9}"
                 +",{\"id\":33,\"username\":\"33\",\"email\":\"33@test.com\",\"department\":null}]";
