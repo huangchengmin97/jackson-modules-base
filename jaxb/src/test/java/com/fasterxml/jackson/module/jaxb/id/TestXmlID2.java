76669633bfff05fa37b8a748d4e25d6f7e4aa466
huangchengmin97 <huangchengmin97@gmail.com>
2021-08-29 19:22:42 -0500

    12

--- jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
+++ jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
@@ -145,10 +145,10 @@ public class TestXmlID2 extends BaseJaxbTest
         // String expected = "[{\"id\":11,\"username\":\"11\",\"email\":\"11@test.com\",\"department\":9}"
         //         +",{\"id\":22,\"username\":\"22\",\"email\":\"22@test.com\",\"department\":9}"
         //         +",{\"id\":33,\"username\":\"33\",\"email\":\"33@test.com\",\"department\":null}]";
-        // String expected =
+        String expected = mapper.writeValueAsString(json);
 
 
-        assertEquals(json, json);
+        assertEquals(json,expected);
 
         // However, there is no way to resolve those back, without some external mechanism...
     }
