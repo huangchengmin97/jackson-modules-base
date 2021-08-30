305beb4799a3e30e447dd9c68e51cb789d9c2383
huangchengmin97 <huangchengmin97@gmail.com>
2021-08-30 17:02:46 -0500

    40

--- jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
+++ jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
@@ -11,7 +11,7 @@ import com.fasterxml.jackson.module.jaxb.BaseJaxbTest;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"id", "username","email"})
+@JsonPropertyOrder({"id", "username", "email" })
 public class TestXmlID2 extends BaseJaxbTest
 {
     @XmlRootElement(name = "department")
