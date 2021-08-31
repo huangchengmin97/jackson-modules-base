eceb178f4b27e02ac0fecde15760355bc6c72725
huangchengmin97 <huangchengmin97@gmail.com>
2021-08-31 00:19:58 -0500

    46

--- jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
+++ jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
@@ -13,7 +13,7 @@ import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 
-@JsonPropertyOrder({"id", "email", "username", "department" })
+
 // @JsonPropertyOrder(alphabetic=true)
 
 public class TestXmlID2 extends BaseJaxbTest
@@ -145,7 +145,7 @@ public class TestXmlID2 extends BaseJaxbTest
         assertEquals(Long.valueOf(33), result.get(2).id);
     }
 
-
+@JsonPropertyOrder({"id", "email", "username", "department" })
     public void testIdWithJaxbRules() throws Exception
     {
         ObjectMapper mapper =  JsonMapper.builder()
