c71cbc20b70294966d30457b6db223288de1541a
huangchengmin97 <huangchengmin97@gmail.com>
2021-08-30 21:58:53 -0500

    46

--- jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
+++ jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
@@ -146,11 +146,11 @@ public class TestXmlID2 extends BaseJaxbTest
 
     public void testIdWithJaxbRules() throws Exception
     {
-        // ObjectMapper mapper =  JsonMapper.builder()
-        //
-        // // but then also variant where ID is ALWAYS used for XmlID / XmlIDREF
-                // .annotationIntrospector(new JaxbAnnotationIntrospector())
-                // .build();
+        ObjectMapper mapper =  JsonMapper.builder()
+
+        // but then also variant where ID is ALWAYS used for XmlID / XmlIDREF
+                .annotationIntrospector(new JaxbAnnotationIntrospector())
+                .build();
         List<User> users = getUserList();
         ObjectMapper mapper =  new ObjectMapper（ ）
         // final String json = mapper.writeValueAsString(users);
